4c92c53882bf58ba4f132155ff9401ab1df3a087
miehmg <cuhkwyszcw@gmail.com>
2018-09-12 16:50:52 +0800

    Add files via upload

---
+++ VRPSimu/src/common.h
@@ -0,0 +1,36 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <math.h>
+#include <random>
+#include <utility>
+#include <memory>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <iterator>
+#include <typeinfo>
+#include <iomanip>
+// #include "2DGraph/BGI_util.h"
+// #include "2DGraph/koolplot.h"
+// #include "2DGraph/PlotData.h"
+// #include "2DGraph/Plotstream.h"
+// #include "2DGraph/winbgitypes.h"
+
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::pair;
+using std::shared_ptr;
+using std::map;
+using std::set;
+using TmSpot = int;
+
+#endif
